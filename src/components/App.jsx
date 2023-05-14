1277429fc1c1e75e5c1c269ddb51d55be2b6fd25
Casimova Marina <maracasimova@gmail.com>
2023-05-14 15:24:57 +0300

    added localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,27 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { nanoid } from 'nanoid';
 import style from './App.module.css';
+import { LOCALSTORAGE_KEY } from './constants';
+
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const storedContacts = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (storedContacts !== null) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = userData => {
     const newUser = { ...userData, id: nanoid() };
